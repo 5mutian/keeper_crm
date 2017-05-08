ebcebc520b0d40bc8720d2cf39159716ebd05d9d
Evan Zhang <evan@EvandeMacBook-Pro.local>
2017-05-08 13:37:56 +0800

    fix role validate

--- app/controllers/api/admin/users_controller.rb
+++ app/controllers/api/admin/users_controller.rb
@@ -3,22 +3,43 @@ class Api::Admin::UsersController < Api::BaseController
 
 	# 列表
 	# 
-	# Params:
+	# Params
 	# 	access_token: [String] authenication_token
 	#   page: [Integer] 页码
-	# Return:
+	# Return
 	# 	status: [String] success
 	#   list: [Hash] {{name, mobile, role}...}
-	# Error:
-	#   info: [String] 自己定义的错误信息
-	#   other: [String] 如果不设置的，将生成默认的
+	# Error
+	#   status: [String] failed
 	def index
 		users = @user.account.users.page(params[:page])
 
 		render json: {status: :success, list: users.map(&:to_hash)}
 	end
 
+	# 创建
+	#
+	# Params
+	# 	access_token: [String] authenication_token
+	# 	user[name]: [String] 用户姓名
+	# 	user[mobile]: [String] 用户手机号
+	# 	user[role]: [String] 用户角色(saler|cs|acct)
+	# 	user[password]: [String] 用户密码
+	# Return
+	# 	status: [String] success
+	# 	msg: [String] 创建成功
+	# Error
+	#   status: [String] failed
+	#   msg: [String] msg_infos	
 	def create
+		user = User.new(params[:user])
+		user.account = @user.account
+
+		if user.save
+			render json: {status: :success, msg: '创建成功'}
+    else
+    	render json: {status: :failed, msg: user.errors.messages.values.first}
+		end
 	end
 
 	def update
--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -7,7 +7,7 @@ class Api::BaseController < ApplicationController
 		# @user = User.where(role: 'Admin').first
 		@user = User.find_by_access_token(params[:access_token])
 		raise '用户不存在' unless @user
-		raise '无权限访问' unless params[:controller].split('/')[1] == @user.role.downcase
+		raise '无权限访问' unless params[:controller].split('/')[1] == @user.role
 
 		rescue => e
 			render json: {status: :failed, msg: e.message}

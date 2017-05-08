30a07968c8c65381867f044ae38a1e4764051eea
Evan Zhang <evan@EvandeMacBook-Pro.local>
2017-05-08 15:57:52 +0800

    add user update

--- app/controllers/api/admin/users_controller.rb
+++ app/controllers/api/admin/users_controller.rb
@@ -32,7 +32,8 @@ class Api::Admin::UsersController < Api::BaseController
 	#   status: [String] failed
 	#   msg: [String] msg_infos	
 	def create
-		user = User.new(params[:user])
+		user = User.new(user_params)
+		user.password = params[:user][:password]
 		user.account = @user.account
 
 		if user.save
@@ -42,9 +43,39 @@ class Api::Admin::UsersController < Api::BaseController
 		end
 	end
 
+	# 更新
+	#
+	# Params
+	# 	access_token: [String] authenication_token
+	# 	user[name]: [String] 用户姓名
+	# 	user[mobile]: [String] 用户手机号
+	# 	user[role]: [String] 用户角色(saler|cs|acct)
+	# Return
+	# 	status: [String] success
+	# 	msg: [String] 更新成功
+	# Error
+	#   status: [String] failed
+	#   msg: [String] msg_infos	
 	def update
+		user = User.find params[:id]
+		raise 'not found' unless user
+		if user.update_attributes(user_params)
+			render json: {status: :success, msg: '更新成功'}
+    else
+    	render json: {status: :failed, msg: user.errors.messages.values.first}
+		end
+
+		rescue => e
+			render json: {status: :failed, msg: e.message}
 	end
 
 	def destroy
 	end
+
+	private
+
+	def user_params
+	 params[:user].permit(:name, :mobile, :role)
+	end
+
 end
--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -4,7 +4,7 @@ class Api::BaseController < ApplicationController
 	skip_before_filter :verify_authenticity_token
 
 	def authenticate_user
-		# @user = User.where(role: 'Admin').first
+		# @user = User.where(role: 'admin').first
 		@user = User.find_by_access_token(params[:access_token])
 		raise '用户不存在' unless @user
 		raise '无权限访问' unless params[:controller].split('/')[1] == @user.role

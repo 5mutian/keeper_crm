f820f3f4da0dd8d403c6be892f9c71cf9d9d0705
张春明 <zhangcm@5mutian.com>
2017-05-22 15:29:36 +0800

    update request header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,26 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :cors_preflight_check
-  after_action :cors_set_access_control_headers
+  before_filter :add_allow_credentials_headers
 
-  private
+	def add_allow_credentials_headers                                                                                                                                                                                                                                                        
+	  # https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS#section_5                                                                                                                                                                                                      
+	  #                                                                                                                                                                                                                                                                                       
+	  # Because we want our front-end to send cookies to allow the API to be authenticated                                                                                                                                                                                                   
+	  # (using 'withCredentials' in the XMLHttpRequest), we need to add some headers so                                                                                                                                                                                                      
+	  # the browser will not reject the response                                                                                                                                                                                                                                             
+	  response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'                                                                                                                                                                                                     
+	  response.headers['Access-Control-Allow-Credentials'] = 'true'                                                                                                                                                                                                                          
+	end
 
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
-    headers['Access-Control-Max-Age'] = "1728000"
-  end
-
-  def cors_preflight_check
-    if request.method == "OPTIONS"
-      headers["Access-Control-Allow-Origin"] = "*"
-      headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS"
-      headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-Prototype-Version, Authorization, Token"
-      headers["Access-Control-Max-Age"] = "1728000"
-      render text: "", content_type: "text/plain"
-    end
-  end
-  
 end

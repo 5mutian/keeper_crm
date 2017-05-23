77d457c96c5262629986be8a8ec085a20d5b5ddf
张春明 <zhangcm@5mutian.com>
2017-05-23 09:33:05 +0800

    update clue

--- app/models/clue.rb
+++ app/models/clue.rb
@@ -2,4 +2,12 @@ class Clue < ActiveRecord::Base
 
 	belongs_to :account
 	belongs_to :user
+	belongs_to :customer
+
+	before_create :gen_customer_id
+
+	def gen_customer_id
+		self.customer_id = 0 unless customer_id
+	end
+
 end

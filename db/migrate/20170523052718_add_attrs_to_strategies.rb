fa0e8db7db52624799a61c7ff8b191a6fe99ed2d
张春明 <zhangcm@5mutian.com>
2017-05-23 13:39:51 +0800

    add some attrs to strategies

--- db/migrate/20170523052718_add_attrs_to_strategies.rb
+++ db/migrate/20170523052718_add_attrs_to_strategies.rb
@@ -1,7 +1,7 @@
 class AddAttrsToStrategies < ActiveRecord::Migration
   def change
-  	add_column :stategies, :title, :string # 主题
-  	add_column :stategies, :discount, :float
-  	add_column :stategies, :rebate, :float
+  	add_column :strategies, :title, :string # 主题
+  	add_column :strategies, :discount, :float
+  	add_column :strategies, :rebate, :float
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522080635) do
+ActiveRecord::Schema.define(version: 20170523052718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -153,6 +153,9 @@ ActiveRecord::Schema.define(version: 20170522080635) do
     t.integer  "account_id",                null: false
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.string   "title"
+    t.float    "discount"
+    t.float    "rebate"
   end
 
   create_table "tokens", force: :cascade do |t|

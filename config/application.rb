cdbb679013fd47f877af178e08ed91b5626db17c
张春明 <zhangcm@5mutian.com>
2017-05-25 11:53:49 +0800

    update application config

--- config/application.rb
+++ config/application.rb
@@ -28,9 +28,9 @@ module KeeperCrm
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :delete, :put, :options]
       end
     end
-    
+
   end
 end

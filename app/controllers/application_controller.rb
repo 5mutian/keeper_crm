a61e75ae5b6cb75b3df9ee6b743c15b6aada6370
张春明 <zhangcm@5mutian.com>
2017-05-25 12:01:01 +0800

    update application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
 
 	def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
     headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
     headers['Access-Control-Max-Age'] = "1728000"
   end
@@ -27,7 +27,7 @@ class ApplicationController < ActionController::Base
   def cors_preflight_check
     if request.method == "OPTIONS"
       headers["Access-Control-Allow-Origin"] = "*"
-      headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS"
+      headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, PATCH, DELETE, OPTIONS"
       headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-Prototype-Version, Authorization, Token"
       headers["Access-Control-Max-Age"] = "1728000"
       render text: "", content_type: "text/plain"

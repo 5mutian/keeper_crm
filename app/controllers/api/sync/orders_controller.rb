ff241b227fcb5e2fbb5b8e252f3abdeeebcc757a
张春明 <zhangcm@5mutian.com>
2017-06-13 17:44:53 +0800

    update order

--- app/controllers/api/sync/orders_controller.rb
+++ app/controllers/api/sync/orders_controller.rb
@@ -6,67 +6,67 @@ class Api::Sync::OrdersController < Api::Sync::BaseController
 	# Return
 	# Error
 	def update_cgj
-		order = Order.find_by_serial_number(params["order"]["serial_number"])
+		_user = params["results"].delete("customer_info")
 
-		if order
-			order.update_attributes({
-				uuid: 										params["order"]["uuid"],
-				width: 										params["order"]["width"],
-				height:         					params["order"]["height"],
-				rate:  										params["order"]["rate"],
-				total: 										params["order"]["total"],
-				remark: 									params["order"]["remark"],
-				state:  									params["order"]["state"],
-				province:   							params["order"]["province"],
-				city:       							params["order"]["city"],
-				area:       							params["order"]["area"],
-				courier_number: 					params["order"]["courier_number"],
-				install_date:   					Time.at(params["order"]["install_date"].to_i),
-				cgj_company_id: 					params["order"]["company_id"],
-				cgj_facilitator_id: 			params["order"]["facilitator_id"],
-				cgj_customer_service_id: 	params["order"]["customer_service_id"],
-				material: 								params["order"]["material"],
-				material_id: 							params["order"]["material_id"],
-				workflow_state: 					params["order"]["workflow_state"],
-				public_order: 						params["order"]["public_order"],
-				square: 									params["order"]["square"],
-				mount_order:  						params["order"]["mount_order"],
-				serial_number: 						params["order"]["serial_number"],
-				is_company: 							params["order"]["is_company"],
-				measure_amount: 					params["order"]["measure_amount"],
-				install_amount:     			params["order"]["install_amount"],
-				manager_confirm: 					params["order"]["manager_confirm"],
-				terminal_count: 					params["order"]["terminal_count"],
-				amount_total_count: 			params["order"]["amount_total_count"],
-				basic_order_tax: 					params["order"]["basic_order_tax"],
-				measure_amount_after_comment: 	params["order"]["measure_amount_after_comment"],
-				installed_amount_after_comment: params["order"]["installed_amount_after_comment"],
-				measure_comment: 								params["order"]["measure_comment"],
-				measure_raty: 									params["order"]["measure_raty"],
-				installed_raty: 								params["order"]["installed_raty"],
-				service_measure_amount: 				params["order"]["service_measure_amount"],
-				service_installed_amount: 			params["order"]["service_installed_amount"],
-				basic_tax: 											params["order"]["basic_tax"],
-				deduct_installed_cost: 					params["order"]["deduct_installed_cost"],
-				deduct_measure_cost: 						params["order"]["deduct_measure_cost"],
-				sale_commission: 								params["order"]["sale_commission"],
-				intro_commission: 							params["order"]["intro_commission"],
-			})
+		unless user
+			company = Company.find_by_cgj_id(_user["company_id"])
 
-			order.customer.update_attributes({
-				longitude: 	params["order"]["longitude"],
-				latitude: 	params["order"]["latitude"],
-				address: 		params["order"]["address"],
-				tel:        params["order"]["tel"],
-				province:   params["order"]["province"],
-				city:       params["order"]["city"],
-				area:       params["order"]["area"],
-				street:  		params["order"]["street"]
-			})
-			render json: {status: :success, msg: 'updated successfully'}
-		else
-			render json: {status: :failed, msg: 'not found'}
+			unless company
+				company 					= Company.find_or_initialize_by(name: _user["company_name"])
+				company.parent_id = @account.id
+				company.cgj_id 		= _user["company_id"]
+				company.save
+			end
+
+			user = User.find_or_initialize_by(mobile: _user["tel"])
+
+			user.name   			= _user["real_name"]
+			user.password 		= 'CRM123'
+			user.role 				= 'admin'
+			user.account_id 	= company.id
+			user.cgj_user_id 	= params["results"]["customer_id"]
+			user.save
 		end
+
+		customer = Customer.find_or_initialize_by(tel: params["results"]["tel"])
+
+		if customer.new_record?
+
+			%w(name province city area street address longitude latitude).each do |ele|
+				customer.attributes[ele] = params["results"][ele]
+			end
+			# customer.name 			= params["results"]["name"]
+			# customer.province 	= params["results"]["province"],
+			# customer.city  			= params["results"]["city"],
+			# customer.area 			= params["results"]["area"],
+			# customer.street 		=	params["results"]["street"]
+			# customer.address 		= params["results"]["address"]
+			# customer.longitude 	= params["results"]["longitude"]
+			# customer.latitude 	= params["results"]["latitude"]
+			customer.user_id 		= user.id
+			customer.account_id = @account.id
+			customer.save
+		end
+
+
+		order = Order.find_or_initialize_by(serial_number: params["results"]["serial_number"])
+
+		(order.attributes.keys & params["results"].keys).each do |ele|
+			order.attributes[ele] = params["results"][ele]
+		end
+
+		order.install_date 						= Time.at(params["results"]["install_date"].to_i)
+		order.cgj_company_id 					= params["results"]["company_id"]
+		order.cgj_facilitator_id 			= params["results"]["facilitator_id"]
+		order.cgj_customer_service_id = params["results"]["customer_service_id"]
+
+		order.user_id 		= user.id
+		order.account_id 	= @account.id
+		order.customer_id = customer.id
+
+		order.save
+
+		render json: {status: :success, msg: 'updated successfully'}
 		
 	end
 

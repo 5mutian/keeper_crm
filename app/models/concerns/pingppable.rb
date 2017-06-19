6ab9e70c37ede577be3657b14bace9dd8733536c
张春明 <zhangcm@5mutian.com>
2017-06-19 11:34:59 +0800

    update payment

--- app/models/concerns/pingppable.rb
+++ app/models/concerns/pingppable.rb
@@ -3,14 +3,14 @@ module Pingppable
   extend ActiveSupport::Concern
 
    class_methods do
-     #查询所有交易
+     # 查询所有交易
      def pingpp_query_all(options={})
        options = {limit: 10, starting_after: nil, ending_before: nil, created: nil, channel: nil, paid: true, refunded: false}.merge(options).compact
        Pingpp::Charge.all(options)
      end
    end
 
-   #支付
+  # 支付
   def pingpp_charge(pay_orders={})
     charge = Pingpp::Charge.create({:app => {"id" => ENV["pingpp_app_id"]}}.merge(pay_orders))
     update(charge_id: charge.id)
@@ -18,7 +18,7 @@ module Pingppable
   end
 
 
-  #退款
+  # 退款
   def pingpp_refund(options={})
     options = {amount: (self.price * 100).to_i, description: self.reason}.merge(options)
     charge = pingpp_charge_type
@@ -26,20 +26,27 @@ module Pingppable
   end
 
 
-    #查询一个订单的退款
+  # 查询一个订单的退款
   def pingpp_charge_refund(options={})
     options = {limit: 10, starting_after: nil, ending_before: nil}.merge(options).compact
     pingpp_charge_type.refunds.all(options)
   end
 
-  #查询一个交易状态
+  # 查询一个交易状态
   def pingpp_charge_type
     Pingpp::Charge.retrieve(self.charge_id)
   end
 
-  #查询一个退款
+  # 查询一个退款
   def pingpp_query_refund
     pingpp_charge_type.refunds.retrieve(self.refund_id)
+  end
+
+  # 企业付款(提现)
+  def pingpp_transfer(transfer_hash={})
+    transfer = Pingpp::Transfer.create({:app => {"id" => ENV["pingpp_app_id"]}}.merge(transfer_hash))
+    update(charge_id: transfer.id)
+    return transfer
   end
 
 end
--- app/models/wallet_log.rb
+++ app/models/wallet_log.rb
@@ -1,7 +1,6 @@
 class WalletLog < ActiveRecord::Base
-  include Pingppable
 
-	# trade_type: alipay, wx
+  include Pingppable
 
 	belongs_to :user
 
@@ -18,7 +17,47 @@ class WalletLog < ActiveRecord::Base
     -1 => 'withdraw_failed' # 当pingXX反回结果失败的话生成一个记录，将提现失败的金额返给钱包
   }
 
+  validates :trade_type, inclusion: {in: [nil, 'alipay', 'wx'], message: '不在所选范围之内'} 
+
   validates_numericality_of :amount, greater_than_or_equal_to: 0.01, message: "流水金额大于0.01"
   validates_numericality_of :total, greater_than_or_equal_to: 0, message: "帐户总额必须大于等于0"
+
+
+  # 支付/充值
+  def generate_pay(remote_ip="127.0.0.1")
+    charge = self.pingpp_charge(payment_options(remote_ip))
+    charge
+  end
+
+  # 提现
+  def generate_withdraw
+    transfer = self.pingpp_transfer(withdraw_options)
+    transfer
+  end
+
+  def p_hash
+    {
+      order_no: "wallet#{id}",
+      channel:  trade_type,
+      amount:   amount * 100,
+      currency: "cny"
+    }
+  end
+
+  def payment_options(remote_ip="127.0.0.1")
+    p_hash.merge(
+      subject:  '支付/充值',
+      body:     '',
+      client_ip: remote_ip
+    )
+  end
+
+  def withdraw_options
+    p_hash.merge(
+      type:     "b2c",
+      recipient: user.open_id,
+      description: "提现到微信"
+    )
+  end
 
 end

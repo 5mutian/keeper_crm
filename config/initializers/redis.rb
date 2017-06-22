0e7202668e20e6b86e9d017f5774e1409177bb00
张春明 <zhangcm@5mutian.com>
2017-06-22 11:44:54 +0800

    update reids

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,7 @@
-  $redis = Redis.new(host: "0.0.0.0", port: "6379", db: 15)
+  $redis = Redis::Namespace.new(
+    ENV["redis_namespace"],
+    redis: Redis.new(
+      host: ENV["redis_server"], 
+      port: ENV["redis_port"],
+      password: ENV["redis_pass"],
+      db: 12))

b833c17a1b9a92cc5835b8428a6de83cda2baa7c
张春明 <zhangcm@5mutian.com>
2017-07-04 14:59:14 +0800

    fix callback

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,7 +1,25 @@
 class PaymentsController < ApplicationController
 
 	def callback
-		
+		if params['is_success'] == 'T'
+			wlog = WalletLog.find_by_id(params['out_trade_no'].delete('wallet'))
+			finish_reward(wlog)
+			render text: "success"
+		else
+			render text: "failed"
+		end
+	end
+
+
+	private
+
+	def finish_reward(wlog)
+		WalletLog.transaction do
+      tip = Tip.find_by_id(wlog.transfer_id)
+      raise ActiveRecord::RecordNotFound if tip.nil?
+      # update_deposit_log
+      wlog.update!(state: 1, total: (wlog.user.wallet_total + wlog.amount).round(2))
+    end
 	end
 
 end
